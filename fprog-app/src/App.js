75b08b591270ded856505945d0e48213858c0ab0
jcwang <jcwang@sonicwall.com>
2022-03-22 16:42:38 +0800

    Init learning space that Functional Programming with JavaScript.

--- fprog-app/src/App.js
+++ fprog-app/src/App.js
@@ -1,35 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-var p = () => {
-  console.log("\nNew Line:");
-}
-
-var msg = "In JavaScript, functions are veriables";
-var log = function(message) {
-  console.log(message);
-}
-log(msg);
-p();
-
-const log2 = (message) => {
-  console.log(message);
-}
-log2(msg);
-p();
-
-const obj = {
-  message: msg,
-  log3(message) {
-    console.log(message);
-  }
-};
-
-obj.message = "Welcome";
-obj.log3(obj.message);
-
-
-
 function App() {
   return (
     <div className="App">
